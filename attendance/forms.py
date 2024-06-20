8f602883837cc43954ff92e6eb8aede7d95896fc
Flavio Macedo <fhenrique609@gmail.com>
2024-06-20 08:49:48 -0300

    Add Placeholder

--- attendance/forms.py
+++ attendance/forms.py
@@ -6,11 +6,13 @@ class PunchForm(forms.ModelForm):
         model = Punch
         fields = ['punch_in_time', 'punch_out_time']
         widgets = {
-            'punch_in_time': forms.TextInput(attrs={
-                'class': 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+            'punch_in_time': forms.DateTimeInput(attrs={
+                'class': 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline',
+                'placeholder': 'YYYY-MM-DD HH:MM:SS', # Ajuste isso conforme o formato desejado
             }),
-            'punch_out_time': forms.TextInput(attrs={
-                'class': 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+            'punch_out_time': forms.DateTimeInput(attrs={
+                'class': 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline',
+                'placeholder': 'YYYY-MM-DD HH:MM:SS', # Ajuste isso conforme o formato desejado
             })
         }
 
